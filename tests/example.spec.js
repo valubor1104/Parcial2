df202a17272435ba7ab02626eed7930fadbb634a
Valeria Bonilla <vbonillar@miumg.edu.gt>
2025-09-20 12:58:10 -0600

    feat: proyecto Playwright + CI con reportes

---
+++ tests/example.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('Verificar título de Playwright', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+
